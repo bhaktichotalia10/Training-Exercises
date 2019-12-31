b96f075905c3e72a81bad2cbb48d088ac25c23e7
bhaktichotalia10 <59190361+bhaktichotalia10@users.noreply.github.com>
2019-12-31 13:11:05 +0530

    Add files via upload

---
+++ JDBC/TestConnection.java
@@ -0,0 +1,27 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestConnection {
+
+	Connection con;
+	public TestConnection()
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "seed1234");
+			System.out.println("Connected To Database....");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new TestConnection();
+	}
+
+}
